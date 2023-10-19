59a9319e3aa589886d9500c197e287b1eaeb8341
dausbel <9dausbel_pavo@singaporeair.com.sg>
2023-10-19 16:57:49 +0700

    Udacity first project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import Footer from "./components/Footer.js";
+import Bookshelf from "./components/Bookshelf";
 
-function App() {
+import { useHistory } from "react-router-dom";
+
+const bookshelves = [
+  { title: "Currently Reading", shelfName: "currentlyReading" },
+  { title: "Want to Read", shelfName: "wantToRead" },
+  { title: "Read", shelfName: "read" }
+];
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    BooksAPI.getAll().then(booksFromApi => {
+      setBooks(booksFromApi);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {bookshelves.map((bookshelf, index) => (
+              <Bookshelf
+                key={index}
+                title={bookshelf.title}
+                books={
+                  books &&
+                  books.filter(
+                    book => book && book.shelf === bookshelf.shelfName
+                  )
+                }
+                setBooks={setBooks}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="open-search">
+          <button onClick={() => history.push("/search")}>Add a book</button>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
